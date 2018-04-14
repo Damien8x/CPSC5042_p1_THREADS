b4e28ea4fdf47f06c1e5ca7266ba140d4977bbb9
Damien8x <damien.sudol@gmail.com>
2018-04-14 14:07:40 -0700

    Functional prototype. Needs further testing
    Now accepts multiple files.
    Takes user input for directory.

--- vowels.cpp
+++ vowels.cpp
@@ -1,11 +1,15 @@
 #include<pthread.h>
-#include<cstring>
+#include<string>
 #include<fstream>
 #include<iostream>
+#include<queue>
+#include<sstream>
 
 using namespace std;
 
 void * countVowels(void *vs);
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 struct vowelStruct
 {
@@ -13,6 +17,9 @@ struct vowelStruct
 int vowelArr[5];
 int count;
 pthread_t id[20];
+string path;
+string line;
+queue <string> fileID;
 
 }; 
 
@@ -21,44 +28,95 @@ int main()
 struct vowelStruct *vs;
 vs = new struct vowelStruct;
 
-for(int i = 0; i < 20; i++)
-{
-	pthread_create(&vs->id[i], NULL, countVowels,(void*) vs);
-}
-while(vs->count!=20){}
-for(int i = 0; i < 5; i++)
-{
-	cout << vs->vowelArr[i] << endl;
-}
+
+
+cout << "enter path to directory containing files." << endl;
+cout << "if in current directory just hit enter." << endl;
+getline(cin, vs->path);
+	if(vs->path == "")
+	{
+		for(int i = 1; i < 21; i++)
+		{
+			string fileValue = to_string(i);
+			string finalPath ="";
+			finalPath =vs->path+ "file"+fileValue+".txt";
+			cout<<finalPath<<endl;
+			vs->fileID.push(finalPath);
+		}
+	}
+	else
+	{
+		for(int i = 1; i < 21; i++)
+		{
+			string fileValue = to_string(i);
+			string finalPath ="";
+			finalPath =vs->path+ "/file"+fileValue+".txt";
+			cout<<finalPath<<endl;
+			vs->fileID.push(finalPath);
+		}
+	}
+	
+	for(int i = 0; i < 20; i++)
+	{
+		pthread_create(&vs->id[i], NULL, countVowels,(void*) vs);
+	}
+	while(vs->count!=20){}
+
+
+	for(int i = 0; i < 5; i++)
+	{
+		if(i == 0)
+			cout << "a:\t"<< vs->vowelArr[i] << endl;
+		else if(i == 1)
+			cout << "e:\t"<< vs->vowelArr[i] << endl;
+		else if(i == 2)
+			cout << "i:\t"<< vs->vowelArr[i] << endl;
+		else if(i == 3)
+			cout << "o:\t"<< vs->vowelArr[i] << endl;
+		else
+			cout << "u:\t"<< vs->vowelArr[i] << endl;
+
+	}
 return 0;
 }
 
 
 void * countVowels(void *vs)
 {
-string line;
-struct vowelStruct * vsp = (struct vowelStruct *) vs;
-ifstream inFile;
-inFile.open("file1.txt");
-while(inFile.good())
-{
-getline(inFile, line);
-for(int i=0; i<line.size(); i++)
-{
-if(line[i] == 'a' || line[i] =='A')
-	vsp->vowelArr[0] += 1; 
-else if(line[i] == 'e' || line[i] == 'E')
-	vsp->vowelArr[1] += 1;
-else if(line[i] == 'i' || line[i] == 'I')
-	vsp->vowelArr[2] += 1;
-else if(line[i] == 'o' || line[i] == 'O')
-	vsp->vowelArr[3] += 1;
-else if(line[i] == 'u' || line[i] == 'U')
-	vsp->vowelArr[4] += 1;
 
-}
-vsp->count++;
-return (void*)vsp;
-}
+struct vowelStruct * vsp = (struct vowelStruct *) vs;
+
+ifstream inFile;
+
+pthread_mutex_lock(&mutex);
+
+string currentFile = vsp->fileID.front();
+
+inFile.open(currentFile.c_str());
+
+vsp->fileID.pop();
+
+pthread_mutex_unlock(&mutex);
+
+	while(inFile.good())
+	{
+		getline(inFile, vsp->line);
+		for(int i=0; i<vsp->line.size(); i++)
+		{
+			if(vsp->line[i] == 'a' || vsp->line[i] =='A')
+				vsp->vowelArr[0] += 1; 
+			else if(vsp->line[i] == 'e' || vsp->line[i] == 'E')
+				vsp->vowelArr[1] += 1;
+			else if(vsp->line[i] == 'i' || vsp->line[i] == 'I')
+				vsp->vowelArr[2] += 1;
+			else if(vsp->line[i] == 'o' || vsp->line[i] == 'O')
+				vsp->vowelArr[3] += 1;
+			else if(vsp->line[i] == 'u' || vsp->line[i] == 'U')
+				vsp->vowelArr[4] += 1;
+
+		}
+	}	
+	vsp->count++;
+	return (void*)vsp;
 
 }
